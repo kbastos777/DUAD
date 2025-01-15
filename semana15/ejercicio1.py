638411917f23669c40c6ee152061a7e5e6a2bf1d
kbastos777 <bastosk21@gmail.com>
2025-01-15 17:36:58 -0600

    Correcciones ejecicio1 y ejercicio extra

--- semana15/ejercicio1.py
+++ semana15/ejercicio1.py
@@ -1,18 +1,18 @@
 def sort_list (list_to_be_sorted):
     for out_index in range((len(list_to_be_sorted)-1),0,-1):
         has_made_changes = False
-        print(list_to_be_sorted)
         for inside_index in range((len(list_to_be_sorted)-1),0,-1):
             current_index = list_to_be_sorted[inside_index]
             next_index = list_to_be_sorted[inside_index-1]
             if current_index < next_index:
                 list_to_be_sorted[inside_index]=next_index
                 list_to_be_sorted[inside_index-1]=current_index
-                has_made_changes = True
-            out_index = out_index - 1    
-    if not has_made_changes:
-        return
-            
+                has_made_changes = True    
+        if not has_made_changes:
+            return
+    return list_to_be_sorted
+
 
 list_of_numbers = [10,58,-5,10,2,6]
 sort_list(list_of_numbers)
+print(list_of_numbers)
--- semana15/ejercicio_extra_alg_ordenamiento.py
+++ semana15/ejercicio_extra_alg_ordenamiento.py
@@ -2,39 +2,52 @@ class Node:
     data: int
     next: "Node"
 
+
     def __init__(self, data, next=None):
         self.data = data
         self.next = next
 
+
 class LinkedList:
     head: Node
+
 
     def __init__(self, head):
         self.head = head
 
-    def print_sorted_structure(self):
-            current_node = self.head
-            number_list=[]
-            while (current_node is not None):
-                    number_list.append(current_node.data)
-                    current_node = current_node.next
-            for out_index in range(0,len(number_list)-1):
-                has_made_changes = False
-                print(number_list)
-                for inside_index in range(0,len(number_list)-1):
-                    current_index = number_list[inside_index]
-                    next_index = number_list[inside_index + 1]
-                    if current_index < next_index:
-                        number_list[inside_index]=next_index
-                        number_list[inside_index+1]=current_index
-                        has_made_changes = True
-                    out_index = out_index + 1    
-                if not has_made_changes:
-                    return
+
+    def sort_structure(self):
+        if not self.head or not self.head.next:
+            return
+
+        swapped = True
+        while swapped:
+            current = self.head
+            swapped = False
+            while current.next:
+                if current.data > current.next.data:
+                    current.data, current.next.data = current.next.data, current.data #Aca aplique algo que se llama desempaquetado multiple
+                    swapped = True
+                current = current.next
+
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
 
 third_node = Node(45)
 second_node = Node(75, third_node)
 first_node = Node(1, second_node)
 
+
 linked_list = LinkedList(first_node)
-linked_list.print_sorted_structure()
+print("Unsorted list:")
+linked_list.print_list()
+
+linked_list.sort_structure()
+print("Sorted list:")
+linked_list.print_list()
